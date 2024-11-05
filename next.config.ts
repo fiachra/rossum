417139ea0e311cdbec8fd5577bd5f3b9a492428a
Fiachra Matthews <fiachra@medable.com>
2024-11-05 17:06:43 +0000

    docker build changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   }
